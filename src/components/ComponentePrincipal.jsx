7edb56166bbfc8eb3230bc2f75950e0fcec3f2e2
agustin mateos <agustinrotondomateos@gmail.com>
2025-01-04 19:40:08 -0300

    ..

--- src/components/ComponentePrincipal.jsx
+++ src/components/ComponentePrincipal.jsx
@@ -5,31 +5,16 @@ export default function ComponentePrincipal() {
       style={{ backgroundImage: "url('/fondo4.svg')" }}
     >
       {/* Contenido dentro del div */}
-      <div className="w-[100%] lg:w-[60%]  h-full flex items-center justify-center">
-        <div className="w-[80%] h-[80%] md:h-[40%] flex flex-col justify-around  ">
-          <div className="text-[40px]"> <h1>Tu aliado en calidad y variedad desde 1965:</h1></div>
-       <div className="text-[32px]"><h2>Cottonier se ha consolidado como un líder en la producción y comercialización de hilos para coser, ofreciendo calidad y variedad para el mercado textil argentino. </h2></div>
+      <div className="w-[100%] lg:w-[60%]  h-full flex items-center  justify-start md:justify-center">
+        <div className=" w-[90%] md:w-[80%]  bg-white/50 backdrop-blur sm:bg-transparent rounded-r-[20px] p-4  md:h-[50%] h-[60%] flex flex-col justify-around  ">
+          <div className= "text-[30px] md:text-[40px]"> 
+            <h1>Tu aliado en calidad y variedad desde 1965:</h1>
+          </div>
+       <div className="text-[20px] md:text-[32px]"><h2>Cottonier se ha consolidado como un líder en la producción y comercialización de hilos para coser, ofreciendo calidad y variedad para el mercado textil argentino. </h2></div>
         
       </div></div>
       
     </div>
   );
 }
-    // {/* {<div
-    //       className="flex flex-col md:flex-row items-center justify-center w-full h-screen bg-cover bg-center"
-    //       style={{ backgroundImage: "url('/fondo2.svg')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}
-    //     >
-    //       {/* Contenido del componente */}
-    //       <div className="relative w-full md:w-1/2 text-center md:text-left p-6 z-10">
-    //         <h2 className="text-2xl md:text-4xl font-bold text-white">
-    //           Tu aliado en calidad y variedad desde 1965
-    //         </h2>
-    //         <p className="mt-4 text-white md:max-w-[450px]">
-    //           Desde 1965, Cottonier SRL se ha consolidado como un líder en la 
-    //           producción y comercialización de hilos para coser, ofreciendo calidad
-    //           y variedad para el mercado textil argentino. Nuestra experiencia y 
-    //           compromiso nos permiten satisfacer las necesidades de nuestros clientes
-    //           con productos de excelencia y un servicio confiable.
-    //         </p>
-    //       </div>
-    //     </div>} */}
+    
